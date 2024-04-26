02f39125f9a4212c9762a0d7bd432b30c233f1e5
salczii <kacper.salek32@gmail.com>
2024-04-26 11:20:06 +0200

    feat: Add welcome message from AWS in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const App = () => {
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
+        <p>Welcome from AWS!</p>
       </div>
       <Outlet />
     </div>
