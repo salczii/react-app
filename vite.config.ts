f0f6ba93808b9a1f2a9375744f38a9fe2a016cc6
salczii <kacper.salek32@gmail.com>
2024-04-27 20:47:55 +0200

    Update environment variables in deployment workflows

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import packageJson from './package.json';
 
+// console.log('VITE_APP_VERSION:', import.meta.env.VITE_APP_VERSION);
+
 export default defineConfig({
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
