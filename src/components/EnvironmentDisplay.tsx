bd6b0f8e26acf045bf654cea822551f0b58dcd0c
salczii <kacper.salek32@gmail.com>
2024-04-29 16:10:22 +0200

    Update CloudFront distribution ID in deployment workflow

--- src/components/EnvironmentDisplay.tsx
+++ src/components/EnvironmentDisplay.tsx
@@ -4,11 +4,14 @@ export const EnvironmentDisplay = () => {
   const appEnvironment = import.meta.env.VITE_ENVIRONMENT;
   const appVersion = __APP_VERSION__;
 
+  if (appEnvironment === 'development') {
+    return null;
+  }
+
   return (
     <div className="bg-amber-100 rounded w-fit p-4 absolute right-0 m-6">
       <p>
         ENV: {appEnvironment} - VER: {appVersion}
-        nowy
       </p>
     </div>
   );
